373bf779526d5c0bd9012c141fd2d3d7229fd31b
R.Wießner <robert.wiessner1@gmx.de>
2015-06-10 10:16:45 +0200

    Basepointer hinzugefügt

--- src/tcb.c
+++ src/tcb.c
@@ -13,9 +13,13 @@
  * wird der kontext geladen geht es hinter diesem Befehl weiter (wegen IP) 
  */
 int tcb_getcontext(tcb *t){
-	//  int long ip;
-	//  __asm__ __volatile__ ( "mov %%eip, %%eax":"=a" (ip)); // speichere ip in ip
-	return 0;
+	__asm__ __volatile__ ( "mov %%esp, %%eax":"=a" (t.stack.ss_sp)); //  Stackpointer gespeichert
+	__asm__ __volatile__ ( "mov %%ebp, %%eax":"=a" (t.tcb_bp)); //  Stackpointer gespeichert
+	
+	
+	__asm__ __volatile__ ( "mov %%eip, %%eax":"=a" (t.tcb_ip)); // speichere ip in ip muss letzte aktion sein.
+ 	// hier gehts dann weiter, wenn der TCB wieder geladen wird.
+ 	return 0;
 }
 
 
@@ -24,7 +28,9 @@ int tcb_getcontext(tcb *t){
  * der IP neu gesetzt wird
  */
 int tcb_setcontext(const tcb *t){
-
+	
+	
+	
 	return 0;
 }
 
@@ -40,5 +46,7 @@ int tcb_swapcontext(tcb *ourTcb, tcb *newTcb){
  * erzeugt einen Neuen Kontext mit der übergebenen FUnktion. Wenn der Kontext aufgerufen wird wird die FUnktion ausgeführt.
  */
 int tcb_makecontext(tcb *t, void *func(), int argc, ...){
+	// malloc der stacksize zum erzeugen des Stacks
+	
 	return 0;
 }
--- src/tcb.h
+++ src/tcb.h
@@ -8,6 +8,7 @@ struct tcb_str;	//forward declaration
 typedef struct tcb_str {
 	long tcb_ip ; 	// instruktion pointer
 					// register speichern
+	long tcb_bp ;   // basepointer
 	//fill this struct with statusinformations
 	stack_t	stack;	//stack for local vars
 	/* rest */
